eeae329774363adbb7d9da4fd37831a012c8e41c
henryueta <henrykenjiueta@gmail.com>
2025-04-20 22:35:19 -0300

    websocketFilesAdd

---
+++ index.js
@@ -0,0 +1,73 @@
+import { WebSocketServer } from "ws";
+import http from "http"
+
+const server = http.createServer();
+
+const wss = new WebSocketServer({
+    server
+})
+
+const connections = new Map();
+let waiting = null;
+
+wss.on('connection',(ws,req)=>{
+    if(waiting == null){
+        waiting = ws;
+    console.log("esperando outro jogador")
+    ws.send(JSON.stringify({
+        player1:"connectado",
+        player2:"esperando . . ."
+    }))
+    } else {
+        const gameId = Date.now().toString()
+        const game = {
+            gameId:gameId,
+            player1:waiting,
+            player2:ws
+        }
+        ws.send(JSON.stringify({
+            player1:"connectado",
+            player2:"connectado"
+        }))
+        console.log("fim da espera")
+        connections.set(game.gameId,game)
+
+        ws.gameId = game.gameId
+        waiting.gameId = game.gameId;
+        waiting = null;
+
+        console.log("o id da partida é "+ws.gameId)
+    }
+    
+    ws.on('message',data=>{ 
+
+        const game = connections.get(ws.gameId)
+
+        ws === game.player2 &&
+        game.player1.send(JSON.stringify({
+            message: data.toString()
+        }))
+        ws === game.player1 &&
+        game.player2.send(JSON.stringify({
+            message: data.toString()
+        }))
+    }
+
+    
+    )
+
+    ws.on('close',(code,reason)=>{
+        connections.delete(ws.gameId)
+        if (code === 1000) {
+            console.log('Cliente desconectado normalmente.');
+        } else {
+            console.log(`A conexão com o cliente foi interrompida inesperadamente. Código: ${code}, Motivo: ${reason}`);
+        }
+
+    })
+
+})
+
+server.listen(3200,()=>{
+    console.log("success")
+})
